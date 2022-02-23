8f28c8920359ba112374ac08a8823c9a21e2d4c4
Ivan Lobanov <jackspow18@gmail.com>
2022-02-23 11:26:16 +0300

    lob_finish

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,5 +1,10 @@
 public class TestClass {
   public static void main(String[] args){
 	System.out.println("master");
+	System.out.println("lob1");
+	System.out.println("lob2");
+	System.out.println("lob3");
+	System.out.println("lob4");
+	System.out.println("lob5");
   }
 }
