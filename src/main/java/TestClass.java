90c51a57fa23970f6bb76dcfefcbd2f81140fe0d
Ivan Lobanov <j2>
2022-04-11 12:18:02 +0300

    commit new

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,13 +1,6 @@
 public class TestClass {
   public static void main(String[] args){
 	System.out.println("master");
-	System.out.println("master1");
-	System.out.println("lob1");
-	System.out.println("lob2");
-	System.out.println("lob3");
-	System.out.println("lob4");
-	System.out.println("lob5");
-	System.out.println("max1");
   }
 
   public void cherry(){
